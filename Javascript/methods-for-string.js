b685daf88e34fc9739226af795b069aee5c612c8
MohammedSameed <mohammedsameed07@gmail.com>
2023-02-07 16:15:39 +0530

    some methods for strings

---
+++ Javascript/methods-for-string.js
@@ -0,0 +1,13 @@
+
+
+let firstname="  sameed  ";
+
+console.log(firstname.length);
+
+let newstring = firstname.trim();
+
+console.log(newstring);
+
+console.log(newstring.length);
+
+console.log(firstname.toUpperCase());
