e834eefca7613311e68d8bb19be9cbc4649194cf
MohammedSameed <mohammedsameed07@gmail.com>
2023-02-07 22:52:31 +0530

    typeof in js

--- Javascript/methods-for-string.js
+++ Javascript/methods-for-string.js
@@ -11,3 +11,12 @@ console.log(newstring);
 console.log(newstring.length);
 
 console.log(firstname.toUpperCase());
+
+
+let age = 22;
+
+let secondname="mohammed";
+
+console.log(typeof age);
+
+console.log(typeof secondname);
