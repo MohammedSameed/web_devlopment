52f43808321969fecbe22d7c0160e35e93ea7d10
MohammedSameed <mohammedsameed07@gmail.com>
2023-02-14 23:07:15 +0530

    nested if else

---
+++ Javascript/nested_if_else.js
@@ -0,0 +1,11 @@
+let winningNumber = 19;
+let userguess = prompt("guess a number ");
+//12
+
+if(winningNumber===userguess){
+    console.log("your guess is correct")
+
+}
+else {
+    console.log("your guessis wrong");
+}
