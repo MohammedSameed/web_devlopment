18bb504669c7ad16778d432150e8667910b20bb3
MohammedSameed <mohammedsameed07@gmail.com>
2023-02-16 23:20:48 +0530

    else if

--- Javascript/temp_degree.js
+++ Javascript/temp_degree.js
@@ -5,11 +5,9 @@ if(tempIndegree<0){
 }
 else if(tempIndegree<16){
     console.log("cold outside");
-
 }
 else if (tempIndegree<25){
     console.log("whether is okay outside");
-
 }
 else if (tempIndegree<35){
     console.log("lets go for swim");
@@ -17,7 +15,6 @@ else if (tempIndegree<35){
 }
 else if (tempIndegree<45){
     console.log("turn on the ac");
-
 }
 else{
     console.log("its too hot ");
