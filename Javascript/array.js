e994446d567494322691ff860ae158235725a5ac
MohammedSameed <mohammedsameed07@gmail.com>
2023-02-19 19:59:56 +0530

    arrays push pop

---
+++ Javascript/array.js
@@ -0,0 +1,9 @@
+
+
+let fruits= ["appple" , "mango" , "grapes"];
+console.log(fruits);
+
+fruits[1] = "banana";
+
+
+console.log(fruits);
---
+++ Javascript/array_push_pop.js
@@ -0,0 +1,12 @@
+let fruits= ["apple" , "banana" , "orange"];
+
+fruits.push("mango");
+
+ console.log (fruits.pop());
+
+console.log (fruits);
+
+fruits.unshift("papaya");
+
+console.log(fruits);
+
