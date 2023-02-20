5d37dfae9363dee72b15d0d144116bb996e7ecfe
MohammedSameed <mohammedsameed07@gmail.com>
2023-02-20 23:21:05 +0530

    const in array

--- Javascript/array_push_pop.js
+++ Javascript/array_push_pop.js
@@ -11,3 +11,14 @@ fruits.unshift("papaya");
 
 console.log(fruits);
 
+
+
+const fruits2=["apple", "mango" , "grapes"];
+
+const fruits3=[];
+let i =0;
+while(i<fruits2.length){
+    fruits3.push(fruits2[i].toUpperCase);
+    i++;
+}
+console.log(fruits3)
