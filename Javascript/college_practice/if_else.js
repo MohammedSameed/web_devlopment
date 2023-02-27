27adb06feba48138e34252b6779f1c1629b13767
MohammedSameed <mohammedsameed07@gmail.com>
2023-02-27 23:17:15 +0530

    if else

---
+++ Javascript/college_practice/if_else.js
@@ -0,0 +1,23 @@
+//let age = 17;
+
+if(age>18){
+    console.log("user can play");
+}
+else{
+    console.log("user can play mario");  
+}
+
+let num =2;
+
+if(num%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+let firstname = "sameed";
+let age = 22;
+
+if (firstname[0]=="s" && age>18){
+    console.log("name start with s and above 18")
+}
