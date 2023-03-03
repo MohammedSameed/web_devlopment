00518dd7859c96afdf249ea6756989e56af5b3d8
MohammedSameed <mohammedsameed07@gmail.com>
2023-03-03 22:18:31 +0530

    temp

---
+++ Javascript/college_practice/temp.js
@@ -0,0 +1,21 @@
+let tempIndegree = 40;
+
+if(tempIndegree<0){
+    console.log("too cold outside");
+}
+else if(tempIndegree<16){
+    console.log("cold outside");
+}
+else if (tempIndegree<25){
+    console.log("whether is okay outside");
+}
+else if (tempIndegree<35){
+    console.log("lets go for swim");
+
+}
+else if (tempIndegree<45){
+    console.log("turn on the ac");
+}
+else{
+    console.log("its too hot ");
+}
